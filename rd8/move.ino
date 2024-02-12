64d1a7a508a8eafc976fbc99f5035e6985d8ef91
Kevin Johnson <kscottjohnson@hotmail.com>
2024-02-11 22:20:34 -0600

    variable speed static walk

--- rd8/move.ino
+++ rd8/move.ino
@@ -64,8 +64,11 @@ float sCurrentX[4] = {0,0,0,0}, sCurrentY[4] = {70,70,70,70};
 float    sPrevX[4] = {0,0,0,0},    sPrevY[4] = {70,70,70,70};
 bool sFirstStep = true;
 int8_t sDirection = 1; // 1 forward, -1 reverse
-uint8_t sLegSpeed = 3; 
-uint8_t sWalkStyle = 1; // 0 - walk, 1 - trot
+uint8_t sLegSpeed = 3; // multiplier for leg movement
+uint8_t sWalkStyle = 0; // 0 - walk, 1 - trot
+int8_t sWalkSpeed = 0;
+int8_t sMaxWalkSpeed = 8;
+
 const uint8_t sLegStateOffset[2][4] = { 
   {0,2,1,3}, // walk
   {0,2,2,0}  // trot
@@ -80,33 +83,83 @@ const int8_t sWalkY[32] = {
 };
 
 void staticWalk() { // defines individual static movement for each leg
+  
+  if(sTick == 0){ // get walk speed at start of walk state
+    sWalkSpeed = map(ly, 0, 255, -1 * sMaxWalkSpeed, sMaxWalkSpeed);
 
-  //moving
-  for(uint8_t l=0; l<4; l++){
-    uint8_t legState = sWalkState + 4 * sLegStateOffset[sWalkStyle][l];
-    if(sTick + 1 == sTicksPerState){ // if this is the last tick just set the targets
-      sCurrentX[l] = sWalkX[legState]*sLegSpeed;
-      sCurrentY[l] = sWalkY[legState];
-      sPrevX[l] = sCurrentX[l];
-      sPrevY[l] = sCurrentY[l];
+    if(sWalkSpeed != 0){
+      sTicksPerState =  (float) sMaxWalkSpeed / abs(sWalkSpeed) * 4;
+      if(sWalkSpeed > 0){
+        sDirection = 1;
+      }
+      else{ 
+        sDirection = -1;
+      }
+      if(abs(sWalkSpeed) > 4){ // trot if speed >= 5
+        sWalkStyle = 1;
+      }
+      else{ 
+        sWalkStyle = 0;
+      }
     }
-    else{
-      sCurrentX[l] += (sWalkX[legState]*sLegSpeed - sPrevX[l]) / sTicksPerState;
-      sCurrentY[l] += (sWalkY[legState] - sPrevY[l]) / sTicksPerState;
+    else {
+      sTicksPerState = 16; // for stationary spin
     }
-    legs[l].move(sCurrentX[l], sCurrentY[l]);
+
   }
   
-  if(++sTick == sTicksPerState){ // new state
+  if(sWalkSpeed == 0){
+
+    for(uint8_t l=0; l<4; l++){
+      sCurrentX[l] = 0;
+      sCurrentY[l] = 70;
+      sPrevX[l] = 0;
+      sPrevY[l] = 70;
+
+      legs[l].move(sCurrentX[l], sCurrentY[l]);
+    }
+    sTick = 0;
+    sWalkState = 0;
+    sDirection = 1;
+    sFirstStep = true;
+  }
+  else{
+    //moving
+    //Serial.print(sWalkState);Serial.print(" ");Serial.println(sTick);
+    for(uint8_t l=0; l<4; l++){
+      uint8_t legState = sWalkState + 4 * sLegStateOffset[sWalkStyle][l];
+      if(sTick + 1 == sTicksPerState){ // if this is the last tick just set the targets
+        sCurrentX[l] = sWalkX[legState]*sLegSpeed;
+        sCurrentY[l] = sWalkY[legState];
+        sPrevX[l] = sCurrentX[l];
+        sPrevY[l] = sCurrentY[l];
+      }
+      else{
+        sCurrentX[l] += (sWalkX[legState]*sLegSpeed - sPrevX[l]) / sTicksPerState;
+        sCurrentY[l] += (sWalkY[legState] - sPrevY[l]) / sTicksPerState;
+      }
+      if(sFirstStep && legState < 9){
+        // hack to make non-lifting leg not go forward when starting
+        sCurrentX[l] = 0; 
+        sPrevX[l] = 0;
+      }
+      legs[l].move(sCurrentX[l], sCurrentY[l]);
+    }
+
+    if(++sTick == sTicksPerState){ // new state
       sTick = 0;
       sWalkState += sDirection;
+      sFirstStep = false;
       if(sWalkState == 16){ // new cycle
         sWalkState = 0;
       }
-      else if(sWalkState == -1){ // new cycle
+      else if(sWalkState == 255){ // new cycle
         sWalkState = 15;
       }
+    }
   }
+
+  
 
 }
 
